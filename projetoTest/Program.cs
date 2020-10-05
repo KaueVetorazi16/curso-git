bff5b59cd0b45ccff6c651413e4930cd1618c708
Kaue Vetorazi <vetorazikaue@hotmail.com>
2020-10-05 08:58:19 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projetoTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projetoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+
+            Console.ReadLine();
+        }
+    }
+}
